e7b4421353569afd435d7a0cb07c56d1b34d2000
AmitpaulCNS <paul.amit.cns@gmail.com>
2023-11-10 13:44:10 +0530

    update code

---
+++ src/Services/AccountServices.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const API_HOST = process.env.REACT_APP_API_HOST;
+
+class AccountService {
+
+}
+
+export default new AccountService();
---
+++ src/Services/whiteLabelServices.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const API_HOST = process.env.REACT_APP_API_HOST;
+
+class WhiteLabelService {
+   
+}
+
+export default new WhiteLabelService();
