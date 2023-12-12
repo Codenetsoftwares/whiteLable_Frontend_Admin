cd1541db4c22ff901e6b138275ea53b1535fb03a
Kaushik-0407 <kaushikbhowmick108@gmail.com>
2023-12-12 11:18:36 +0530

    done

--- src/Components/MyAccount/ActivityLog.jsx
+++ src/Components/MyAccount/ActivityLog.jsx
@@ -3,53 +3,39 @@ import React from "react";
 const ActivityLog = () => {
   return (
     <div className="col-sm-8 mt-3">
-      <div
-        className="card_box position-relative mb_30"
-        style={{ backgroundColor: "#ffc717" }}
-      >
-        <div className="box_body">
-          <div class="main_content_iner rounded ">
-            <div class="container-fluid ">
-              <div class="row justify-content-center">
-                <div class="white_card_body mt-5 pt-5">
-                  <div class="QA_section">
-                    <div class="QA_table mb_30">
-                      <table class="table lms_table_active3 ">
-                        <thead>
-                          <tr>
-                            <th scope="col">Login Date & Time</th>
-                            <th scope="col">Login Status</th>
-                            <th scope="col">IP Address</th>
-                            <th scope="col">ISP</th>
-                            <th scope="col">City/State/Country</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <th scope="row">{Date()}</th>
-                            <td>Active</td>
-                            <td>45.250.48.146</td>
-                            <td>Wish Net</td>
-                            <td>Kolkata</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-2">
-                  <div class="loader--ellipsis colord_bg_4 mb_30">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+      {/* card */}
+      <div class="card w-100 rounded">
+        <div
+          class="card-heade text-white p-1"
+          style={{ backgroundColor: "#26416e" }}
+        >
+          <b>&nbsp;&nbsp;Activity Log</b>
         </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item p-3">
+            <table class="table table-bordered table-responsive-sm table-responsive-md table-responsive-lg table-responsive-xl">
+              <thead>
+                <tr class="table-active">
+                  <th scope="col">Login Date & Time</th>
+                  <th scope="col">Login Status</th>
+                  <th scope="col">IP Address</th>
+                  <th scope="col">ISP</th>
+                  <th scope="col">City/State/Country</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td scope="row"></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </li>
+        </ul>
       </div>
+      {/* card */}
     </div>
   );
 };
